0c1cda5fb98a4f55463ef898fa53b04e63e36925
daaavus <daaavus@gmail.com>
2023-02-06 09:43:54 +0300

    first commit

---
+++ ex7/ex7.js
@@ -0,0 +1,21 @@
+jQuery(function ($) {
+    $(document).ready(function () {
+        $(".slider").slick({
+            arrows: true,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 560,
+                    settings: {
+                        arrows: true,
+                        dots: true,
+                        slidesToScroll: 1,
+                        slidesToShow: 2
+                    }
+                }
+            ],
+            slidesToScroll: 1,
+            slidesToShow: 4
+        });
+    });
+});
